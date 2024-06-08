d81472c0d832b600f2a2eef979bc00e7273e9e70
Nikhil Karthik <nikhilkarthik241103@gmail.com>
2024-06-09 03:38:55 +0530

    Portfolio completed

--- components/Landing.tsx
+++ components/Landing.tsx
@@ -1,3 +1,4 @@
+import { Calistoga } from "next/font/google";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
@@ -5,9 +6,9 @@ import { GiHamburgerMenu } from "react-icons/gi";
 
 export default function Landing() {
   return (
-    <section className="p-4 bg-white h-screen flex flex-col justify-center w-full md:px-10 lg:px-20 xl:px-44">
+    <section className="p-4 bg-white h-screen flex flex-col justify-center w-full">
       <nav className="w-full p-4 md:px-10 lg:px-20 xl:px-44 z-10 text-black flex justify-between items-center absolute top-0 left-0">
-        <div className="text-2xl font-semibold">Bazil</div>
+        <div className="text-2xl font-semibold">Alex Smith</div>
         <div className="md:flex hidden gap-8 items-center">
           <Link
             href={"/"}
@@ -31,7 +32,7 @@ export default function Landing() {
             href={"/"}
             className="text-sm p-4 bg-black text-white hover:px-8 hover:shadow-2xl transition-all  rounded duration-300"
           >
-            hello@bazil.fr
+            hello@aslex.en
           </Link>
         </div>
         <div className="text-black text-xl flex md:hidden">
@@ -39,25 +40,36 @@ export default function Landing() {
         </div>
       </nav>
 
-      <div className="flex text-black flex-col relative items-center pt-32 md:pt-44 h-full">
+      <div className="flex text-black flex-col relative items-center pt-16 md:pt-44 h-full">
         <div className="flex flex-col items-center justify-center">
-          <div className="text-[clamp(18px,3vw,26px)] text-center">
-            My name is Bazil and I am a freelancer
+          <div className="text-[clamp(14px,3vw,26px)] text-center">
+            My name is Alex Smith and I am a freelancer
           </div>
           <div className="relative title">
-            <div className="duration-500 title-main relative tracking-tight left-0 text-[clamp(48px,10vw,140px)] font-semibold">
-              Web Designer
+            <div className="duration-500 title-main relative tracking-tight left-0 text-[clamp(48px,10vw,180px)] font-semibold">
+              Webdesigner
             </div>
-            <div className="absolute top-0 left-0 pointer-events-none tracking-tight z-[4] h1_outline text-[clamp(48px,10vw,140px)] font-semibold">
-              Web Designer
+            <div className="absolute top-0 left-0 pointer-events-none tracking-tight z-[4] h1_outline text-[clamp(48px,10vw,180px)] font-semibold">
+              Webdesigner
             </div>
           </div>
           <div className="relative heading">
-            <div className="duration-500 heading-main relative leading-10 tracking-tighter left-0 text-[clamp(48px,10vw,140px)] font-semibold">
+            <div className="duration-500 heading-main relative leading-10 tracking-tighter left-0 text-[clamp(48px,10vw,180px)] font-semibold">
               & Photographer
             </div>
-            <div className="absolute top-0 left-0 pointer-events-none leading-10 tracking-tighter z-[4] h2_outline text-[clamp(48px,10vw,140px)] font-semibold">
+            <div className="absolute top-0 left-0 pointer-events-none leading-10 tracking-tighter z-[4] h2_outline text-[clamp(48px,10vw,180px)] font-semibold">
               & Photographer
+            </div>
+          </div>
+          <div className="md:flex hidden w-full justify-center md:justify-start pt-6 md:pt-20 text-xl">
+            <p>from California, USA</p>
+          </div>
+          <div className="gap-4 md:gap-8 flex flex-col items-center md:flex-row z-[5] pt-6 md:pt-40 justify-center">
+            <div className="text-xs p-2 md:text-sm md:p-3 w-fit bg-black text-white hover:px-8 font-semibold hover:shadow-2xl transition-all rounded duration-300">
+              You need a designer
+            </div>
+            <div className="text-xs p-2 md:text-sm md:p-3 w-fit bg-transparent text-black border-2 font-semibold border-black hover:px-8 hover:shadow-2xl transition-all rounded duration-300">
+              You need a photographer
             </div>
           </div>
         </div>
@@ -67,7 +79,7 @@ export default function Landing() {
           width={0}
           sizes="100%"
           alt="profile"
-          className="absolute pointer-events-none z-[3] max-w-full md:w-[50%] w-full top-auto left-auto right-auto bottom-0 align-middle clip-margin inline-block overflow-clip "
+          className="absolute pointer-events-none z-[3] max-w-full md:w-[40%] w-full top-auto left-auto right-auto bottom-0 align-middle clip-margin inline-block overflow-clip "
         />
       </div>
     </section>
